33db8cad145f8ba0b15b08431d7c49737cb4a9bd
Marshall Culpepper <marshall@mozilla.com>
2015-05-12 17:32:18 -0500

    integrate bunyan for json parseable logs

--- ground/lib/log.js
+++ ground/lib/log.js
@@ -1,23 +1,45 @@
+import bunyan from 'bunyan';
 import winston from 'winston';
 winston.cli();
 
-var logger = new winston.Logger({
-  transports: [
-    new winston.transports.File({
-      filename: '/var/log/vanguard-station.log',
-      level: 'debug',
-      timestamp: true
-    }),
-    new winston.transports.Console({
-      colorize: true,
-      level: 'info'
-    })
-  ]
+class WinstonStream {
+  write(rec) {
+    let level = bunyan.nameFromLevel[rec.level].toLowerCase();
+    let msg = rec.msg;
+    delete rec.msg;
+    delete rec.v;
+    delete rec.level;
+
+    winston.log(level, msg, rec);
+  }
+}
+
+function bufferSerializer(buf) {
+  if (!(buf instanceof Buffer)) {
+    return buf;
+  }
+  return { length: buf.length, data: buf.toString('base64') }
+}
+
+var logger = bunyan.createLogger({
+  name: 'station',
+  serializers: {
+    data: bufferSerializer
+  },
+  streams: [{
+    path: '/tmp/vg-station.log',
+    level: 'debug'
+  }, {
+    level: 'info',
+    type: 'raw',
+    stream: new WinstonStream()
+  }]
 });
 
 logger.setVerbosity = function(verbosity) {
   if (verbosity >= 1) {
-    logger.transports.console.level = 'debug';
+    winston.default.transports.console.level = 'debug';
+    logger.levels(1, 'DEBUG');
   }
 }
 
