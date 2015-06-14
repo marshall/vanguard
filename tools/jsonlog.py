217d4e6eaafa03797a68e03d1fb756a65c594958
Jesse Hamner <jhamner@gmail.com>
2015-06-14 01:19:22 -0500

    Create jsonlog.py
    This file takes the converted JSON output from rdb2json.sh and formats it for further analysis. This script is simple enough to not require much documentation or unit tests.

---
+++ tools/jsonlog.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import sys
+import csv
+import json
+import unicodedata
+import re
+import os
+
+# Sample entry:
+# u'{
+#   "free_mem": 436400, 
+#   "int_temp": 16.67024923021694, 
+#   "uptime": 7491.07, 
+#   "cpu_usage": 57, 
+#   "ext_temp": -13.193173533505671
+# }'
+
+bigKeys = ['telemetry', 'location', 'location_session_count']
+singleKeys=['free_mem', 'int_temp', 'uptime', 'cpu_usage', 'ext_temp']
+output="redislog.txt"
+filename = "redis_dump.json"
+
+f = open(filename)
+
+try:
+    fdata = json.load(f)
+except ValueError:
+    print "Value Error occurred: this file may not be properly formatted JSON."
+    raise
+
+#print "fdata type: " +  str(type(fdata))
+wholedoc = fdata[0]
+#print "Length of wholedoc: " + str(len(wholedoc))
+#print "wholedoc type: " + str(type(wholedoc))
+telem=wholedoc['telemetry']
+#print "Length of telem: " + str(len(telem))
+#print str(telem)
+
+g = open(output, 'w')
+
+g.write('index')
+for key in singleKeys:
+    g.write( '\t' + key )
+g.write('\n')
+
+for index in range(0,len(telem)):
+#    print str(telem[index])
+    item = json.loads(telem[index])
+#    print str(item.keys() )
+    g.write(str(index))
+    for key in singleKeys:
+        g.write('\t' + str(item[key]) )
+    g.write('\n')
+
+g.close()
+f.close()
