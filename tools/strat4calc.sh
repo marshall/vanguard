9c39db5f1d517d3108467804d1224c0edbcc73a3
Marshall Culpepper <marshall@mozilla.com>
2015-07-12 22:33:19 -0500

    STRAT-4 flight parameters

---
+++ tools/strat4calc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 600g Pawan from Aether Industries
+# Copied from http://www.aetherandbeyond.com/main/store/product/12-20-ft-dia-professional-weather-balloon-600g-white.html
+#
+# Product Details:
+#
+#    Diameter at release: (1.8 m)
+#    Diameter at burst altitude: (6.1 m)
+#    Nominal Lift: 3.8 lb (1,700 g)
+#    Maximum Lift: (5,450 g)
+#    Burst altitude: 100,000 ft (30,500 m)
+#
+#    Volume at release: 113 cu ft (1.42 cu m)
+#    Neck diameter: 2.8 in ()
+#    Neck length: 9 in ()
+#    Ascent rate: /min (5 m/s)
+#    Material: Latex Rubber
+
+
+
+
+this_dir=$(cd "`dirname "$0"`"; pwd)
+
+cd "$this_dir"
+
+python burstcalc.py \
+    --target-burst-alt 27500 \
+    --payload-mass     1725 \
+    --balloon          p600 \
+    --burst-diameter   6.1 \
+    $@
